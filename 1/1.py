8a103de5b63044757abb9b38ca8dec0c7967c1b0
Artem Reka <kadetar2003@gmail.com>
2023-12-25 00:24:01 +0300

    fixing full path

--- 1/1.py
+++ 1/1.py
@@ -2,40 +2,42 @@ import numpy as py
 import matplotlib.pyplot as plt
 import math as mh
 import numpy as np
+import os
 
-
-#Заданные заданием пределы. Шаг выбран произвольно
+# Заданные заданием пределы. Шаг выбран произвольно
 x_min = -512
 x_max = 512
 dx = 0.1
 A = 512
 
-#Создание массива точек с заданным шагом и их подстановка в функцию y
-x = py.arange(x_min , x_max + 1, dx)
-y = -(A+47)*py.sin(py.sqrt(py.abs((x/2)+(A+47)))) - x*py.sin(py.sqrt(py.abs(x-(A+47))))
+# Создание массива точек с заданным шагом и их подстановка в функцию y
+x = py.arange(x_min, x_max + 1, dx)
+y = -(A + 47) * py.sin(py.sqrt(py.abs((x / 2) + (A + 47)))) - x * py.sin(py.sqrt(py.abs(x - (A + 47))))
 
-#Подсчет количества точек
-num_max = int(((abs(x_max)+abs(x_min))/dx)+1)
+# Подсчет количества точек
+num_max = int(((abs(x_max) + abs(x_min)) / dx) + 1)
 num = py.arange(1, num_max + 1)
 
-#Заталкивание всех точек в один массив
+# Заталкивание всех точек в один массив
 n = 0
 out = []
 while n < num_max:
-  out.append((num[n], x[n], y[n]))
-  n += 1
+    out.append((num[n], x[n], y[n]))
+    n += 1
 
-#out += [[num[n]] + [x[n]] + [y[n]]]
+# out += [[num[n]] + [x[n]] + [y[n]]]
 
-  
+
+# check dir 'result'
+os.mkdir('result') if not os.path.isdir('result') else print('Уже есть такая директрория')
 # Сохраняем результаты в текстовый файл
-with open(r"1/result/result.txt", "w") as file:
+with open(r"./result/result.txt", "w") as file:
     for i in range(len(x)):
         file.write("{:.4f}    {:.4f}, \n".format(x[i], y[i]))
 
-#Построение графика
-plt.plot(x,y)   
-plt.title("Line graph")   
+# Построение графика
+plt.plot(x, y)
+plt.title("Line graph")
 plt.ylabel('Y axis')
 
 plt.xlabel('X axis')
@@ -43,9 +45,10 @@ plt.xlim(x_min, x_max)
 
 plt.show()
 
-#Вывод итоговых значений
+# Вывод итоговых значений
 print(np.array(out))
 print("\nПрограмма завершила свою работу")
+
 
 
 
